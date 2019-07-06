8c38d01308934355a1da67a5ae7463b80784c2f6
Cole Johnson <Colejohnson147@gmail.com>
2019-07-05 18:39:13 -0700

    added second slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,6 +1,6 @@
 var i = 0; 
 var images = [];
-var time = 4000;
+var time = 3000;
 
 // Image List
 images[0] = 'images/cloud-homepage.png';
@@ -25,4 +25,32 @@ function changeImg(){
     setTimeout("changeImg()", time);
 }
 
-window.onload = changeImg;
+
+
+var iTwo = 0; 
+var imagesTwo = [];
+
+// Image List
+imagesTwo[0] = 'images/carinsurance.png';
+imagesTwo[1] = 'images/carinsurance-quote.png';
+imagesTwo[2] = 'images/carinsurance-admin.png';
+
+// Changes Image
+
+function changeImgSecond(){
+    document.slidetwo.src = imagesTwo[iTwo];
+
+    if(iTwo < imagesTwo.length - 1)
+    {
+        iTwo++;
+    }
+    else
+    {
+        iTwo =0;
+    }
+
+    setTimeout("changeImgSecond()", time);
+}
+
+window.onload= changeImg();
+window.onload= changeImgSecond();
