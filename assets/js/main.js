1ae7e2fff60bbaecf8c0d048f8c9cc2820dd20b9
Cole Johnson <Colejohnson147@gmail.com>
2019-12-04 18:20:41 -0800

    changed image folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,10 +230,10 @@ var images = [];
 var time = 3000;
 
 // Image List
-images[0] = 'images/cloud-homepage.png';
-images[1] = 'images/cloud-login.png';
-images[2] = 'images/cloud-nopics.png';
-images[3] = 'images/cloud-pics.png';
+images[0] = 'images/cloud/cloud-homepage.png';
+images[1] = 'images/cloud/cloud-login.png';
+images[2] = 'images/cloud/cloud-nopics.png';
+images[3] = 'images/cloud/cloud-pics.png';
 
 // Changes Image
 
@@ -255,9 +255,9 @@ var iTwo = 0;
 var imagesTwo = [];
 
 // Image List
-imagesTwo[0] = 'images/carinsurance.png';
-imagesTwo[1] = 'images/carinsurance-quote.png';
-imagesTwo[2] = 'images/carinsurance-admin.png';
+imagesTwo[0] = 'images/carinsurance/carinsurance.png';
+imagesTwo[1] = 'images/carinsurance/carinsurance-quote.png';
+imagesTwo[2] = 'images/carinsurance/carinsurance-admin.png';
 
 // Changes Image
 
